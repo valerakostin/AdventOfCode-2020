71f5169861941fc0575cbd6cb51041b8e5b56c9d
Valera Kostin <valera.kostin@gmail.com>
2020-12-06 18:07:41 +0100

    rename and move passing function to utils

--- day4/day4.js
+++ day4/day4.js
@@ -1,11 +1,12 @@
 import { input } from './input.js'
+import { collectNewLineSeparatedEntries as collectNewLineSeparatedEntries } from '../utils/utils'
 
 const day4 = () => {
 
-    const entries = collectEntries();
+    const entries = collectNewLineSeparatedEntries(input);
     const validFields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
     const task1 = () => {
-        
+
         return entries.filter(entry => validFields.every(f => entry.includes(f))
         ).length
     }
@@ -77,31 +78,11 @@ const day4 = () => {
                     }
                 }
             }
-
             if (valid) {
                 numOfVlaid++;
             }
         }
         return numOfVlaid
-    }
-
-    function collectEntries() {
-        const entries = [];
-        let currentLine = ''
-        const lines = input.split('\n');
-        for (let line of lines) {
-            if (line.trim() === '') {
-                entries.push(currentLine);
-                currentLine = '';
-            }
-            else {
-                const str = line.trim();
-                currentLine = currentLine + ' ' + str;
-            }
-        }
-        // Last entry
-        entries.push(currentLine);
-        return entries;
     }
 
     console.log(`Day 4: Passport Processing
