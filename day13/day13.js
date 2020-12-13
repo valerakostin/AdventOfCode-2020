ccff428ab4be4cbc6d844b2fd7e55bdc8fdfd958
Valera Kostin <valera.kostin@gmail.com>
2020-12-13 17:27:55 +0100

    day 13 add brute force solution for part 2

--- day13/day13.js
+++ day13/day13.js
@@ -1,7 +1,7 @@
-import { input } from "./input.js";
+import { input } from './input.js';
 
 const day13 = () => {
-  const data = input.split("\n");
+  const data = input.split('\n');
 
   const waitTime = (n, bus) => {
     const next = Math.ceil(n / bus) * bus;
@@ -10,8 +10,8 @@ const day13 = () => {
   const task1 = () => {
     const earliestTimestamp = +data[0];
     return data[1]
-      .split(",")
-      .filter((bus) => bus !== "x")
+      .split(',')
+      .filter((bus) => bus !== 'x')
       .map((bus) => {
         return { bus: +bus, waitTime: waitTime(earliestTimestamp, +bus) };
       })
@@ -20,13 +20,35 @@ const day13 = () => {
       .reduce((_, a) => a.bus * a.waitTime, 0);
   };
 
-  const task2 = () => {};
+  const task2 = () => {
+    const buses = [];
+    const rawData = data[1].split(',');
+
+    for (let index = 0; index < rawData.length; index++) {
+      const element = rawData[index];
+      if (element !== 'x') buses.push({ bus: +element, offset: index });
+    }
+
+    const maxBus = [...buses].sort((a, b) => b.bus - a.bus)[0];
+    const max = 500000000000000;
+
+    const next = Math.ceil(max / maxBus.bus);
+    const initial = next * maxBus.bus;
+    //const initial = maxBus.bus;
+
+    const offset = maxBus.offset;
+
+    for (let index = initial; ; index = index + maxBus.bus) {
+      if (buses.every((bus) => (index - offset + bus.offset) % bus.bus === 0))
+        return index - offset;
+    }
+  };
 
   console.log(`Day 13: Shuttle Search
 Task 1: ${task1()}
 Task 2: ${task2()} `);
 };
 
-console.time("Time:");
+console.time('Time:');
 day13();
-console.timeEnd("Time:");
+console.timeEnd('Time:');
